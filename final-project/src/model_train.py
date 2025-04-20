31c750d797e076e3510ee595a27c0764e197db75
thompf2 <faydia.thompson@gmail.com>
2025-04-20 16:26:50 -0400

    Update model_train.py

--- final-project/src/model_train.py
+++ final-project/src/model_train.py
@@ -86,7 +86,7 @@ best_model.fit(
 
 # save the trained best model
 import joblib
-joblib.dump(best_model, "best_xgb_model.pkl")
+joblib.dump(best_model, "models/best_xgb_model.pkl")
 
 
 y_pred_proba = best_model.predict_proba(X_test)[:, 1]
